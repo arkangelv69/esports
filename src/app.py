a5d2242d04c14d3d2f4e792ea5616a249afd7db3
dmunoz <dmunoz@opensistemas.com>
2017-10-14 14:28:13 +0200

    añado fichero de configuración

--- src/app.py
+++ src/app.py
@@ -1,17 +1,27 @@
 from neo4j.v1 import GraphDatabase
 import json
 
-uri = "bolt://domain:7687"
-driver = GraphDatabase.driver(uri, auth=("neo4j", "xxxxx"))
+with open('config.json') as json_data_file:
+    cfg = json.load(json_data_file)
+
+print(cfg)
+
+uri = "bolt://"+cfg['database']['domain']+":7687"
+driver = GraphDatabase.driver(uri, auth=(cfg['database']['username'], cfg['database']['password']))
 
 game = "csgo"
 teamLocalSlug = "beacon-esports"
 teamVisitorSlug = "limitless"
 
+scoreGlobal = 0
+scoreLocal = 0
+scoreVisitor = 0
+
 class Team:
-    def __init__(self, attributes, stats):
+    def __init__(self, attributes, stats, country):
         self.attributes = attributes
         self.stats = stats
+        self.country = country
 
 def getTeamBySlugAndGame(slug, game):
     with driver.session() as session:
@@ -19,11 +29,12 @@ def getTeamBySlugAndGame(slug, game):
             query = "MATCH (tm:Team)-[:PLAYS]->(g:Game)<-[:BELONGS]-(gp:GameProvider{slug:{game}}) \
             MATCH (tm)<-[:BELONGS]-(tmp:TeamProvider) \
             MATCH (tm)-[:HAS]->(st:Stat) \
+            MATCH (tm)-[:BELONGS]->(c:Country)<-[:BELONGS]-(cp:CountryProvider) \
             WHERE tmp.slug = {slug} \
-            RETURN tmp,st"
+            RETURN tmp,st,cp"
 
             for record in tx.run(query, slug=slug, game=game):
-                output = Team(record['tmp'],json.loads(record['st']['stats']))
+                output = Team(record['tmp'],json.loads(record['st']['stats']),record['cp'])
                 return output
 
 localTeam = getTeamBySlugAndGame(teamLocalSlug, game)
